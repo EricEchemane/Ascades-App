2f97c225776acf2557f9a0e293490caecb06b63c
Eric Echemane <eechemane29@gmail.com>
2022-11-19 21:14:29 +0800

    custom appbar

--- lib/presentation/widgets/user_widget.dart
+++ lib/presentation/widgets/user_widget.dart
@@ -1,6 +1,5 @@
 import 'package:ascades/model/user_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class UserWidget extends StatefulWidget {
@@ -17,23 +16,38 @@ class _UserWidgetState extends State<UserWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        systemOverlayStyle: const SystemUiOverlayStyle(
-          statusBarColor: Colors.white,
-          statusBarIconBrightness: Brightness.dark,
-          statusBarBrightness: Brightness.dark, // For iOS (dark icons)
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                // padding: const EdgeInsets.all(7),
+                margin: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(15)),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        spreadRadius: 2,
+                        blurRadius: 4,
+                        offset:
+                            const Offset(0, 4), // changes position of shadow
+                      ),
+                    ]),
+                child: ListTile(
+                  leading: GoogleUserCircleAvatar(identity: widget.identity),
+                  title: Text(widget.identity.displayName!),
+                  subtitle: Text(widget.identity.email),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.menu),
+                    onPressed: () {},
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: Column(
-        children: [
-          ListTile(
-            leading: GoogleUserCircleAvatar(identity: widget.identity),
-            title: Text(widget.identity.displayName!),
-            subtitle: Text(widget.identity.email),
-          )
-        ],
       ),
     );
   }
